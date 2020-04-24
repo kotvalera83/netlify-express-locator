934f690ee39a56c9af57b7d8501f2facb2cba536
Valerii Kot <kotvalera@gmail.com>
2020-04-24 08:26:18 +0200

    Update server.js

--- express/server.js
+++ express/server.js
@@ -4,7 +4,11 @@ const path = require('path');
 const serverless = require('serverless-http');
 const app = express();
 const bodyParser = require('body-parser');
-
+const cron = require("node-cron");
+const faunadb = require('faunadb');
+const faunadbKey = 'fnADqC6W_OACCx8gaWh6Rsykbf5uAlq_Y30PCMkv';
+const client = new faunadb.Client({ secret: faunadbKey });
+const q = faunadb.query;
 const router = express.Router();
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
