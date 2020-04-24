e5f57ac0026264ec1606a62fab1795cf7f58846c
Valerii Kot <kotvalera@gmail.com>
2020-04-24 08:31:16 +0200

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -1,5 +1,9 @@
 'use strict';
-
+const cron = require("node-cron");
+const faunadb = require('faunadb');
+const faunadbKey = 'fnADqC6W_OACCx8gaWh6Rsykbf5uAlq_Y30PCMkv';
+const client = new faunadb.Client({ secret: faunadbKey });
+const q = faunadb.query;
 const app = require('./express/server');
 
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
