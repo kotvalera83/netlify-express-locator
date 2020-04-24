84deaf928d9c67e931e2a35d312389106c1da0b5
Valerii Kot <kotvalera@gmail.com>
2020-04-24 08:33:53 +0200

    Update server-local.js

--- server-local.js
+++ server-local.js
@@ -5,5 +5,22 @@ const faunadbKey = 'fnADqC6W_OACCx8gaWh6Rsykbf5uAlq_Y30PCMkv';
 const client = new faunadb.Client({ secret: faunadbKey });
 const q = faunadb.query;
 const app = require('./express/server');
-
+cron.schedule("*/15 * * * *", function() {
+  client.query(
+    q.Map(
+      q.Paginate(
+        q.Match(q.Index("all_cron"))
+      ),
+      q.Lambda("X", q.Get(q.Var("X")))
+    )
+  )
+  .then( function(ret) {
+    for (let index = 0; index < ret.data.length; index++) {
+      const element = ret.data[index];
+      console.log(element.data); // {"zip": 60061}
+      // remove colection
+      client.query(q.Delete(q.Ref(element.ref))).then((r) => console.log(r));
+    }
+  })
+});
 app.listen(3000, () => console.log('Local app listening on port 3000!'));
